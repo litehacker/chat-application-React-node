1e7a1207f8d2ecb41fab7816c124ed6f7ef9dd9d
AzureAD\GiorgiGvimradze <giorgi.gvimradze@solutions2share.net>
2022-06-16 16:15:02 +0400

    initiate nodeJS TS version with frontend react

---
+++ backend/build/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var http_1 = __importDefault(require("http")); // 1 - Import Node.js core module
+var server = http_1.default.createServer(function (req, res) {
+    if (req.url == "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<html><body><p>This is home Page.</p></body></html>");
+        res.end();
+    }
+});
+server.listen(5000); //3 - listen for any incoming requests
+console.log("Node.js web server at port ".concat(5000, " is running.."));
---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import http from "http"; // 1 - Import Node.js core module
+
+var server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.write("<html><body><p>This is home Page.</p></body></html>");
+    res.end();
+  }
+});
+
+server.listen(5000); //3 - listen for any incoming requests
+
+console.log(`Node.js web server at port ${5000} is running..`);
