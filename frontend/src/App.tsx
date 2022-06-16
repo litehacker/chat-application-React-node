c4f1e46fbb3378a112bfd3a9a88fa958b4ffb678
AzureAD\GiorgiGvimradze <giorgi.gvimradze@solutions2share.net>
2022-06-16 19:01:48 +0400

    fuck sockets yet, let me do frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,4 @@
-import React from "react";
-import { io, Socket } from "socket.io-client";
-import {
-  ServerToClientEvents,
-  ClientToServerEvents,
-} from "./InterfaceTypes/intex";
-
 function App() {
-  const [socket, setSocket] = React.useState<
-    Socket<ServerToClientEvents, ClientToServerEvents>
-  >(io("http://localhost:3000", { transports: ["websocket"] }));
-
-  React.useEffect((): any => {
-    return () => socket.close();
-  }, [setSocket]);
-
   return <div className="App">Hello React</div>;
 }
 
