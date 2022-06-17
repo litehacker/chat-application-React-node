7dcea06ca69955244c876ba88ab0a811346394b0
AzureAD\GiorgiGvimradze <giorgi.gvimradze@solutions2share.net>
2022-06-18 00:22:59 +0400

    making rooms by two concept

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,7 @@ import {
 } from "./InterfaceTypes/intex";
 import { createServer } from "http";
 import express from "express";
+import { uuid } from "uuidv4";
 
 const app = express();
 const httpServer = createServer(app);
@@ -17,20 +18,35 @@ const io = new Server(httpServer, {
     origin: "http://localhost:3000",
   },
 });
+let countUsers: number = 0;
+let rooms: UserType[] = [];
 
 io.on("ping", () => {
   console.log("ping, they say");
 });
 
+let roomID: string = "";
 io.on("connection", (socket) => {
   console.log("connection", socket.id);
-  socket.join("private room");
-  socket.on("hello", (payload: UserType) => {
-    console.log("hello, they say", payload.name);
-  });
+  // on order to separate each two users by one room,
+
+  if (countUsers % 2 === 0) {
+    roomID = uuid();
+    countUsers = 0; // reset users
+  }
+
+  console.log(socket.id, " in room #", roomID);
+  countUsers++;
+  socket.join(roomID);
+
   socket.on("message", (payload: MessageType) => {
-    io.to("private room").emit("message", payload);
-    console.log(payload.content, " says ", payload.author.name);
+    io.to(roomID).emit("message", payload);
+    console.log(
+      payload.content,
+      " says ",
+      payload.author.name,
+      payload.author.id
+    );
   });
 });
 
